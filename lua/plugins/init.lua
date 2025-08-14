fc8b4d9653dcfca40dfae415b833b465a52bba8e
Amit-syntax <amitkr20020405@gmail.com>
2025-08-14 15:27:33 +0530

    init nvchad

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,20 +9,65 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
   -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+  { import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc", 
+        "html", "css", "python",
+  		},
+  	},
+  },
+
+  {
+   "williamboman/mason.nvim",
+   opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "stylua",
+      "html-lsp",
+      "css-lsp",
+      "prettier",
+      "pyre", 
+      "gopls",
+    },
+   },
+  },
+{
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = {
+      options = vim.opt.sessionoptions:get(),
+    },
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("VimEnter", {
+        group = vim.api.nvim_create_augroup("RestoreSession", { clear = true }),
+        callback = function()
+          if vim.fn.argc() > 0 or vim.fn.line2byte("$") ~= -1 or not vim.o.modifiable then
+            return
+          end
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "" and vim.api.nvim_buf_get_name(buf) ~= "" then
+              return
+            end
+          end
+          require("persistence").load()
+        end,
+        nested = true,
+      })
+    end,
+  },
 }
